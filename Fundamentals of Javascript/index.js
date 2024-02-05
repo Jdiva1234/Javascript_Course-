ad679a3dfea8a3abc9f9d4567a3c71a9b662d394
Justice <unankajustice@gmail.com>
2024-02-05 08:22:22 +0000

    Fix character count and limit in tweet prompt

--- Fundamentals of Javascript/index.js
+++ Fundamentals of Javascript/index.js
@@ -3,7 +3,9 @@ alert("Hello, " + name + "!");
 
 let tweet = prompt("Compose your tweet");
 let yourCharacter = tweet.length;
-alert("You have written " + yourCharacter + "characters,  you have " + (182- tweet.length) + "charcters left")
+alert("You have written " + yourCharacter + "characters,  you have " + (140- tweet.length) + "charcters left");
+let tweetLess = tweet.slice(0,140);
+alert(tweetLess);
 
 
 /** The different types of functions in JavaScripts are: 
