736ab4c53272e6af542a05f88d736115bfcd1d97
Justice <unankajustice@gmail.com>
2024-02-05 08:15:05 +0000

    Add name prompt and tweet character count- Angela Lesson

--- Fundamentals of Javascript/index.js
+++ Fundamentals of Javascript/index.js
@@ -1,2 +1,10 @@
 var name = prompt("What is your name?");
 alert("Hello, " + name + "!");
+
+let tweet = prompt("Compose your tweet");
+let yourCharacter = tweet.length;
+alert("You have written " + yourCharacter + "characters,  you have " + (182- tweet.length) + "charcters left")
+
+
+/** The different types of functions in JavaScripts are: 
+ */
