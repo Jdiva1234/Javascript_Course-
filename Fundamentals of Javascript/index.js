29736c9b7130927894c47e99d714de74bed18aa2
Justice <unankajustice@gmail.com>
2024-02-06 06:49:11 +0000

    Refactor code and add function types

--- Fundamentals of Javascript/index.js
+++ Fundamentals of Javascript/index.js
@@ -1,12 +1,53 @@
+// var name = prompt("What is your name?");
+// alert("Hello, " + name + "!");
+
+// let tweet = prompt("Compose your tweet");
+// let yourCharacter = tweet.length;
+// alert("You have written " + yourCharacter + "characters,  you have " + (140- tweet.length) + "charcters left");
+// let tweetLess = tweet.slice(0,140);
+// alert(tweetLess);
+// let capital = tweet.toUpperCase();
+
 var name = prompt("What is your name?");
-alert("Hello, " + name + "!");
-
-let tweet = prompt("Compose your tweet");
-let yourCharacter = tweet.length;
-alert("You have written " + yourCharacter + "characters,  you have " + (140- tweet.length) + "charcters left");
-let tweetLess = tweet.slice(0,140);
-alert(tweetLess);
-
+var firstName = name.slice(0,1);
+var upperCase = firstName.toUpperCase();
+var restName = name.slice(1,name.length);
+var together = upperCase + restName;
+alert("Hello, " + together + " !");
+ 
 
 /** The different types of functions in JavaScripts are: 
+    * 1. Function Declaration
+    * 2. Function Expression
+    * 3. Arrow Function
+    * 4. Anonymous Function
+    * 5. IIFE (Immediately Invoked Function Expression)
+    * 6. Generator Function
+    * 7. Recursive Function
+    * 8. Callback Function
+    * 9. Higher Order Function
+    * 10. Pure Function
+    * 11. Impure Function
+    * 12. Asynchronous Function
+    * 13. Synchronous Function
+    * 14. Nested Function
+    * 15. Named Function
+    * 16. Anonymous Function
+    * 17. Constructor Function
+    * 18. Factory Function
+    * 19. Method Function
+    * 20. Static Function
+    * 21. Prototype Function
+    * 22. Class Function
+    * 23. Regular Function
+    * 24. Generator Function
+    * 25. Recursive Function
+    * 26. Callback Function
+    * 27. Higher Order Function
+    * 28. Pure Function
+    * 29. Impure Function 
  */
+
+
+
+ 
